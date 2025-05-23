c62dca3e361a8b289ea3be2185cc60967815e26a
samir0607 <samir.gupta987@gmail.com>
2025-05-24 05:23:47 +0530

    comments added

--- sql-data-warehouse-project/scripts-sqlserver/bronze/proc_load_bronze.sql
+++ sql-data-warehouse-project/scripts-sqlserver/bronze/proc_load_bronze.sql
@@ -36,7 +36,7 @@ BEGIN
 	
 		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
 		BULK INSERT bronze.crm_cust_info
-		FROM '/data/source_crm/cust_info.csv'
+		FROM '/data/source_crm/cust_info.csv' -- Note: Adjust the path as needed
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -52,7 +52,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info
-		FROM '/data/source_crm/prd_info.csv'
+		FROM '/data/source_crm/prd_info.csv' -- Note: Adjust the path as needed
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -68,7 +68,7 @@ BEGIN
 		SET @start_time = GETDATE();
 		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
-		FROM '/data/source_crm/sales_details.csv'
+		FROM '/data/source_crm/sales_details.csv' -- Note: Adjust the path as needed
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -88,7 +88,7 @@ BEGIN
 	
 		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
 		BULK INSERT bronze.erp_cust_az12
-		FROM '/data/source_erp/CUST_AZ12.csv'
+		FROM '/data/source_erp/CUST_AZ12.csv' -- Note: Adjust the path as needed
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -104,7 +104,7 @@ BEGIN
 
 		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
 		BULK INSERT bronze.erp_loc_a101
-		FROM '/data/source_erp/LOC_A101.csv'
+		FROM '/data/source_erp/LOC_A101.csv' -- Note: Adjust the path as needed
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -120,7 +120,7 @@ BEGIN
 	
 		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM '/data/source_erp/PX_CAT_G1V2.csv'
+		FROM '/data/source_erp/PX_CAT_G1V2.csv' -- Note: Adjust the path as needed
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
