b8cd72839623098d070dac20e73804b471f74c84
samir0607 <samir.gupta987@gmail.com>
2025-05-25 01:47:49 +0530

    feat: add date exploration script for analyzing order and customer demographics

---
+++ EDA/date_exploration.sql
@@ -0,0 +1,30 @@
+/*
+===============================================================================
+DML Script: Date Exploration
+===============================================================================
+Script Purpose:
+    This script performs date-related explorations in the Gold layer of the data 
+		warehouse.
+		It provides insights into the order dates and customer birth dates to 
+		understand the business timeline and customer demographics.
+
+Usage:
+    - This script can be executed to retrieve key date-related metrics
+===============================================================================
+*/
+
+-- Find the Dates of first and last orders
+-- Find How many years did the business sold
+SELECT 
+MIN(order_date) AS first_order_date,
+MAX(order_date) AS last_order_date,
+DATEDIFF(months, MIN(order_date), MAX(order_date)) AS order_ranges_months
+FROM gold.fact_sales
+
+-- Find yongest and oldest customer age range
+SELECT
+MIN(birth_date) AS youngest_customer_birthdate,
+DATEDIFF(year, MIN(birth_date), GETDATE()) AS current_oldest_age,
+MAX(birth_date) AS oldest_customer_birthdate,
+DATEDIFF(year, MAX(birth_date), GETDATE()) AS current_youngest_age
+FROM gold.dim_customers
