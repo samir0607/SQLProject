009e11e3953a9edb53a35f03280af78a66cc7066
samir0607 <samir.gupta987@gmail.com>
2025-05-26 02:14:29 +0530

    feat: add SQL script for comprehensive customer report with key metrics and KPIs

---
+++ sql-data-analytics-project/report_customer.sql
@@ -0,0 +1,108 @@
+/*
+===============================================================================
+Customer Report
+===============================================================================
+Purpose:
+    - This report consolidates key customer metrics and behaviors
+
+Highlights:
+    1. Gathers essential fields such as names, ages, and transaction details.
+	2. Segments customers into categories (VIP, Regular, New) and age groups.
+    3. Aggregates customer-level metrics:
+	   - total orders
+	   - total sales
+	   - total quantity purchased
+	   - total products
+	   - lifespan (in months)
+    4. Calculates valuable KPIs:
+	    - recency (months since last order)
+		- average order value
+		- average monthly spend
+===============================================================================
+*/
+
+-- =============================================================================
+-- Create Report: gold.report_customers
+-- =============================================================================
+IF OBJECT_ID('gold.report_customers', 'V') IS NOT NULL
+    DROP VIEW gold.report_customers;
+GO
+
+CREATE VIEW gold.report_customers AS
+WITH base_query AS(
+/*---------------------------------------------------------------------------
+1) Base Query: Retrieves core columns from tables
+---------------------------------------------------------------------------*/
+    SELECT
+        f.order_number,
+        f.product_key,
+        f.order_date,
+        f.sales_amount,
+        f.quantity,
+        c.customer_key,
+        c.customer_number,
+        CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
+        DATEDIFF(year, c.birth_date, GETDATE()) age
+    FROM gold.fact_sales f
+    LEFT JOIN gold.dim_customers c
+    ON c.customer_key = f.customer_key
+    WHERE order_date IS NOT NULL
+)
+
+, customer_aggregation AS (
+/*---------------------------------------------------------------------------
+2) Customer Aggregations: Summarizes key metrics at the customer level
+---------------------------------------------------------------------------*/
+    SELECT 
+        customer_key,
+        customer_number,
+        customer_name,
+        age,
+        COUNT(DISTINCT order_number) AS total_orders,
+        SUM(sales_amount) AS total_sales,
+        SUM(quantity) AS total_quantity,
+        COUNT(DISTINCT product_key) AS total_products,
+        MAX(order_date) AS last_order_date,
+        DATEDIFF(month, MIN(order_date), MAX(order_date)) AS lifespan
+    FROM base_query
+    GROUP BY 
+        customer_key,
+        customer_number,
+        customer_name,
+        age
+)
+
+SELECT
+    customer_key,
+    customer_number,
+    customer_name,
+    age,
+    CASE 
+        WHEN age < 18 THEN 'Under 18'
+        WHEN age between 18 and 29 THEN '18-29'
+        WHEN age between 30 and 41 THEN '30-41'
+        WHEN age between 42 and 53 THEN '42-53'
+        WHEN age between 54 and 65 THEN '54-65'
+        ELSE 'Above 65'
+    END AS age_group,
+    CASE 
+        WHEN lifespan >= 12 AND total_sales > 6000 THEN 'VIP'
+        WHEN lifespan >= 12 THEN 'Regular'
+        ELSE 'New'
+    END AS customer_segment,
+    last_order_date,
+    DATEDIFF(month, last_order_date, GETDATE()) AS recency,
+    total_orders,
+    total_sales,
+    total_quantity,
+    total_products,
+    lifespan,
+    -- Compute average order value (AVO)
+    CASE WHEN total_sales = 0 THEN 0
+         ELSE total_sales/total_orders
+    END AS avg_order_value,
+    -- Compute average monthly spendings
+    CASE WHEN lifespan = 0 THEN total_sales
+         ELSE total_sales/lifespan
+    END AS average_monthly_spends
+    FROM customer_aggregation
