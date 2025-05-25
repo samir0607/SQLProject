aa6e9b3b7f1e259964eb33659339be0e7d122ecc
samir0607 <samir.gupta987@gmail.com>
2025-05-25 22:13:17 +0530

    feat: add SQL script for yearly performance analysis of products

---
+++ sql-data-analytics-project/performance_analysis.sql
@@ -0,0 +1,36 @@
+
+-- Performance Analysis --
+/* Analyze the yearly performance of products by comparing their sales
+to both the average sales performance of the product and the previous year's sales*/
+WITH yearly_product_sales AS (
+SELECT
+YEAR(f.order_date) AS order_year,
+p.product_name,
+SUM(f.sales_amount) AS current_sales
+FROM gold.fact_sales f 
+LEFT JOIN gold.dim_products p 
+ON f.product_key = p.product_key
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(f.order_date), p.product_name
+)
+SELECT 
+order_year,
+product_name,
+current_sales,
+AVG(current_sales) OVER (PARTITION BY product_name) AS avg_sales,
+current_sales - AVG(current_sales) OVER (PARTITION BY product_name) AS diff_avg,
+CASE 
+    WHEN current_sales - AVG(current_sales) OVER (PARTITION BY product_name) > 0 THEN 'Above Avg'
+    WHEN current_sales - AVG(current_sales) OVER (PARTITION BY product_name) < 0 THEN 'Below Avg'
+    ELSE 'Avg'
+END AS avg_change,
+-- year-over-year Analysis
+LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) py_sales,
+current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) diff_py,
+CASE 
+    WHEN current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) > 0 THEN 'Increasing'
+    WHEN current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) < 0 THEN 'Decreasing'
+    ELSE 'Same'
+END AS py_change
+FROM yearly_product_sales
+ORDER BY product_name, order_year
