a8d5cd61ef452af4655d436ed68dbe2460532048
samir0607 <samir.gupta987@gmail.com>
2025-05-26 10:30:27 +0530

    feat: Create view product report with key metrics and segmentation

--- sql-data-analytics-project/report_product.sql
+++ sql-data-analytics-project/report_product.sql
@@ -18,8 +18,88 @@ Highlights:
        - recency (months since last sale)
        - average order revenue (AOR)
        - average monthly revenue
-===============================================================================
+================================================================================
 */
 -- =============================================================================
 -- Create Report: gold.report_products
 -- =============================================================================
+IF OBJECT_ID('gold.report_products', 'V') IS NOT NULL
+    DROP VIEW gold.report_products;
+GO
+
+CREATE VIEW gold.report_products AS
+
+WITH base_query AS (
+/*------------------------------------------------------------------------------
+1) Base Query: Retrieves core columns from gact_sales and dim_products
+------------------------------------------------------------------------------*/
+    SELECT
+    f.order_number,
+    f.order_date,
+    f.customer_key,
+    f.sales_amount,
+    f.quantity,
+    p.product_key,
+    p.product_name,
+    p.category,
+    p.sub_category,
+    p.cost
+    FROM gold.fact_sales f
+    LEFT JOIN gold.dim_products p
+    ON p.product_key = f.product_key
+    WHERE order_date IS NOT NULL
+)
+
+, product_aggregation AS (
+/*------------------------------------------------------------------------------
+2) Aggregation Query: Calculate product level key metrics
+------------------------------------------------------------------------------*/
+    SELECT
+    product_key,
+    product_name,
+    category,
+    sub_category,
+    cost,
+    DATEDIFF(month, MIN(order_date), MAX(order_date)) AS lifespan,
+    MAX(order_date) AS last_sale_date,
+    ROUND(AVG(CAST(sales_amount AS FLOAT)/NULLIF(quantity, 0)), 1) AS avg_selling_price,
+    SUM(quantity) AS total_quantity,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    COUNT(DISTINCT order_number) AS total_orders
+    FROM base_query
+    GROUP BY product_key, product_name, category, sub_category, cost
+)
+/*---------------------------------------------------------------------------
+  3) Last Query: Generate final report
+---------------------------------------------------------------------------*/
+SELECT
+product_key,
+    product_name,
+    category,
+    sub_category,
+    cost,
+    last_sale_date,
+    DATEDIFF(month, last_sale_date, GETDATE()) AS recency_in_months,
+    CASE 
+        WHEN total_sales > 50000 THEN 'High-Performer'
+        WHEN total_sales >= 10000 THEN 'Average-Performer'
+        ELSE 'Low-Performer'
+    END AS product_segment,
+    lifespan,
+    avg_selling_price,
+    total_quantity,
+    total_sales,
+    total_customers,
+    total_orders,
+    -- Average Order Revenue (AOR)
+    CASE 
+        WHEN total_orders = 0 THEN 0
+        ELSE total_sales/total_orders
+    END AS avg_order_revenue,
+    -- Average Monthly Revenue
+    CASE 
+        WHEN lifespan = 0 THEN total_sales
+        ELSE total_sales / lifespan
+    END AS avg_monthly_revenue
+FROM product_aggregation
